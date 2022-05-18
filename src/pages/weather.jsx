b1cf8637358a08863e559ea34448ca8d0f11f593
Nadav Shalti <snadav12@gmail.com>
2022-05-18 20:36:58 +0300

    mixed content

--- src/pages/weather.jsx
+++ src/pages/weather.jsx
@@ -133,6 +133,7 @@ export default function Weather() {
     };
     return (
         <div className="weather">
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
             <form onSubmit={searchCity}>
                 <input
                     type="search"
